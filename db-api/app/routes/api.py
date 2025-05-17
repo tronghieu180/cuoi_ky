fbaeed15d192cbc490858f90753e0b083dfa0fdf
DoanHuynhNgocSon_21115053120140 <doanhuynhngocson123@gmail.com>
2025-05-17 19:04:14 +0700

    add products api

--- db-api/app/routes/api.py
+++ db-api/app/routes/api.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import insert
 from typing import Dict, List, Any
 from app.database.models import Phone, TabletDevice, Accessory
 from app.database.database import get_db
+from app.database.schemas import PhoneSearch, TabletDeviceSearch, AccessorySearch
+from fastapi.middleware.cors import CORSMiddleware
 
 router = APIRouter()
+
+router.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @router.get("/phone")
 async def get(db: Session = Depends(get_db)):
@@ -66,3 +76,57 @@ def upload(model, data_list: List[dict], db: Session = Depends(get_db)):
     db.commit()
     
     return "Ok"
+    
+@router.get("/phones", response_model=List[PhoneSearch])
+def get_phones(db: Session = Depends(get_db)):
+    return db.query(Phone).all()
+
+@router.get("/tabletdevices", response_model=List[TabletDeviceSearch])
+def get_tabletdevices(db: Session = Depends(get_db)):
+    return db.query(TabletDevice).all()
+
+@router.get("/accessories", response_model=List[AccessorySearch])
+def get_accessories(db: Session = Depends(get_db)):
+    return db.query(Accessory).all()
+
+@router.get("/phones/{phone_id}", response_model=PhoneSearch)
+def get_phone(phone_id: int, db: Session = Depends(get_db)):
+    phone = db.query(Phone).filter(Phone.id == phone_id).first()
+    if not phone:
+        raise HTTPException(status_code=404, detail="Phone not found")
+    return phone
+
+@router.get("/tabletdevices/{tabletdevice_id}", response_model=TabletDeviceSearch)
+def get_tabletdevice(tabletdevice_id: int, db: Session = Depends(get_db)):
+    tabletdevice = db.query(TabletDevice).filter(TabletDevice.id == tabletdevice_id).first()
+    if not tabletdevice:
+        raise HTTPException(status_code=404, detail="Tablet Device not found")
+    return tabletdevice
+
+@router.get("/accessories/{accessory_id}", response_model=AccessorySearch)
+def get_accessory(accessory_id: int, db: Session = Depends(get_db)):
+    accessory = db.query(Accessory).filter(Accessory.id == accessory_id).first()
+    if not accessory:
+        raise HTTPException(status_code=404, detail="Accessory not found")
+    return accessory
+
+@router.get("/search_phones", response_model=List[PhoneSearch])
+def search_phones(query: str, db: Session = Depends(get_db)):
+    return db.query(Phone).filter(
+        Phone.title.ilike(f"%{query}%") |
+        Phone.category.ilike(f"%{query}%")
+    ).all()
+
+@router.get("/search_tabletdevices", response_model=List[TabletDeviceSearch])
+def search_tabletdevices(query: str, db: Session = Depends(get_db)):
+    return db.query(TabletDevice).filter(
+        TabletDevice.title.ilike(f"%{query}%") |
+        TabletDevice.category.ilike(f"%{query}%")
+    ).all()
+
+@router.get("/search_accessories", response_model=List[AccessorySearch])
+def search_accessories(query: str, db: Session = Depends(get_db)):
+    return db.query(Accessory).filter(
+        Accessory.title.ilike(f"%{query}%") |
+        Accessory.category.ilike(f"%{query}%")
+    ).all()
