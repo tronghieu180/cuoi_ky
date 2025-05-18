cda7da41f13428441c3fa64a53feab42c47f0bd5
Vantienhuynh <Vantienhuynh2105@gmail.com>
2025-05-18 20:23:55 +0700

    frontend and api for search

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchProducts from './Component/SearchProducts'
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    <SearchProducts />
+
+  )
+}
+
+export default App
